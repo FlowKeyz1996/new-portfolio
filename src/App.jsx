837347dba153e31766665fb0621e78e5c4cfb8b7
Sanni Emmanuel <sanniemmanuel@macbooks-MacBook-Pro.local>
2023-04-07 11:44:20 +0100

    added imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
+import { BrowserRouter } from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarCanvas } from './components';
 
 const App = () => {
 
 
   return (
-    <div>
-     flowkeyzDEV
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+
+      </div>
+ wahala
     </div>
+    </BrowserRouter>
+    
   )
 }
 
