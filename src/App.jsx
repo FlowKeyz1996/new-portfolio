bf1f31221fb6e73bbb5044e1fcf6c64cef3a6317
Sanni Emmanuel <sanniemmanuel@macbooks-MacBook-Pro.local>
2023-04-07 12:28:11 +0100

    added the navbar to existing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarCanvas } from './components';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
 
@@ -8,14 +8,24 @@ const App = () => {
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar/>
-        <Hero/>
-
+      <Navbar/>
+      <Hero/>
       </div>
- wahala
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+  
+     
     </div>
+    <div className="relative z-0">
+      <Contact/>
+      <StarsCanvas/>
+
+    </div>
+
     </BrowserRouter>
-    
   )
 }
 
